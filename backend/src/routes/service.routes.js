ae58a573d5ec3cf30d17bc52f044e02158556ab6
KaranVikas <karanvikas15@gmail.com>
2024-11-08 18:41:37 -0500

    feat(services): implement endpoints for service lifecycle management

--- backend/src/routes/service.routes.js
+++ backend/src/routes/service.routes.js
@@ -1,9 +1,18 @@
 import { Router } from 'express'
-import {} from '../controllers/service.controller.js'
+import { createService,
+  getAllServices,
+  getServiceById,
+  updateService,
+  deleteService} from '../controllers/service.controller.js'
 
 const router =  Router()
 
-router.route('/').get(getAllServices);
+router.route('/create').post(createService);
+router.route('/getAllServices').get(getAllServices);
 router.route('/:serviceId').get(getServiceById);
+router.route('/:serviceId').put(updateService);
+router.route('/:serviceId').delete(deleteService);
+
+
 
 export default router
