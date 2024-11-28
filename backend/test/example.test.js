25a0761a953a419957a8d3460ade1ea061b7fd78
kongchenglc <kongchenglc@gmail.com>
2024-11-28 17:36:16 -0500

    feat: test case

--- backend/test/example.test.js
+++ backend/test/example.test.js
@@ -1,7 +1,26 @@
-import assert from 'assert';
+import * as chai from 'chai'
+import chaiHttp from 'chai-http'
+import { Service } from '../src/models/services.model.js'
 
-describe('Example Test', () => {
-  it('should return true', () => {
-    assert.strictEqual(true, true);
-  });
-});
+chai.use(chaiHttp);
+
+const {expect} = chai
+
+describe('Services Controller Tests', () => {
+  const serviceData = {
+    name: "Deep Cleaning",
+    description: "Comprehensive cleaning of all rooms",
+    basePrice: 100,
+    duration: "3 hours"
+  };
+
+  describe("POST /createService", () => {
+    it("should create a new service", async() => {
+      const req = {
+        body: {
+
+        }
+      }
+    })
+  })
+})
